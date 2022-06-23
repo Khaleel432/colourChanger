cc8cbd19be352c0e1d30b1917dfe1d8c725c6179
Khaleel <khaleelanjum0@gmail.com>
2022-06-23 20:39:27 +1000

    Implemented array object

--- colourChanger.js
+++ colourChanger.js
@@ -2,7 +2,17 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function changeBackgroundColour() {
+//Select a colour word from an array of colours
+function colourFromArray() {
+    const colours = ["white","silver","gray","black","red","maroon","yellow","olive","lime","green","aqua","teal","blue","navy","fuchsia","purple"];
+    const randomNum = Math.floor(Math.random() * 15);
+    const randomColour = colours[randomNum];
+    document.body.style.background = randomColour;
+    document.getElementById("displayText").innerHTML = `Background Colour: ${randomColour}`;
+}
+
+//Generate a random colour using Math.floor() and Math.random()
+async function randomColour() {
     const seconds = 0.5;
     for(let i = 0; i< 15; i++) {
         let randomColour = "#" + Math.floor(Math.random() * 16777215).toString(16);
